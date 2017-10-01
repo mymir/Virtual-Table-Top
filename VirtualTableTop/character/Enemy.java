b200dfeb5fd37d66ecff04d0649e66921a8d56cf
Rafael Zingle <mymir.zingle@gmail.com>
2017-10-01 08:52:26 -0400

    Added Enemy, deleted Character folder

--- VirtualTableTop/character/Enemy.java
+++
@@ -1,9 +0,0 @@
-package virtualtabletop.character;
-
-public class Enemy extends Character {
-
-	public Enemy(String name, int armourClass,int hitPoints, int currentHitPoints, int strength, int initiativeBonus) {
-		super(name, armourClass, hitPoints, currentHitPoints, strength, initiativeBonus);
-	}
-
-}
---
+++ VirtualTableTop/src/virtualtabletop/character/Enemy.java
@@ -0,0 +1,9 @@
+package virtualtabletop.character;
+
+public class Enemy extends Character {
+
+	public Enemy(String name, int armourClass,int hitPoints, int strength, int initiativeBonus, String weapon) {
+		super(name, armourClass, hitPoints, strength, initiativeBonus, weapon);
+	}
+
+}
